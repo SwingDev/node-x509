44e47d305b67b5f84b82e11d9998aefd96ab4b09
Tomek Kopczuk <tkopczuk@gmail.com>
2017-10-09 20:00:11 +0200

    Further removal of all unused functions from the compiled binary, and modifying tests to support only the 'raw string' certificate passing method.

--- test/test.js
+++ test/test.js
@@ -7,34 +7,6 @@ var x509 = require('../index'),
 // Simple enough test, no?
 fs.readdirSync(path.join(__dirname, 'certs')).forEach(function (file) {
   console.log("File: %s", file);
-  console.log(x509.parseCert(path.join(__dirname, 'certs', file)));
-  // x509.parseCert(path.join(__dirname, 'certs', file));
+  console.log(x509.parseCert(fs.readFileSync(path.join(__dirname, 'certs', file)).toString('binary')));
   console.log();
 });
-
-
-x509.verify(
-  path.join(__dirname, 'certs/enduser-example.com.crt'),
-  path.join(__dirname, 'CA_chains/enduser-example.com.chain'),
-  function (err) {
-    assert.strictEqual(err, null);
-  }
-);
-
-
-
-x509.verify(
-  path.join(__dirname, 'certs/acaline.com.crt'),
-  path.join(__dirname, 'CA_chains/enduser-example.com.chain'),
-  function (err, result) {
-    assert.throws(assert.ifError.bind(null, err), /unable to get local issuer/)
-  }
-);
-
-x509.verify(
-  path.join(__dirname, 'certs/notexisting.com.crt'),
-  path.join(__dirname, 'CA_chains/enduser-example.com.chain'),
-  function (err, result) {
-    assert.throws(assert.ifError.bind(null, err), /ENOENT/)
-  }
-);
